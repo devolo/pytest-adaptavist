c114685558c5264b38e5ef2a502dc99e716f47dc
Markus Bong <Markus.Bong@devolo.de>
2021-10-12 05:19:39 +0000

    fix typo

--- tests/test_init.py
+++ tests/test_init.py
@@ -89,7 +89,7 @@ def test_adaptavist_reporting(pytester: pytest.Pytester, adaptavist: Tuple[Magic
 
 @pytest.mark.usefixtures("adaptavist")
 def test_unknown_user(pytester: pytest.Pytester):
-    """Test the correct behaviour of an unknonwn user."""
+    """Test the correct behaviour of an unknown user."""
     pytester.makepyfile("""
         import pytest
 
