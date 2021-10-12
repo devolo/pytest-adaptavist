a6ba69c3a8c0316a1b45774182e4fa2fe644f3ab
Markus Bong <Markus.Bong@devolo.de>
2021-10-12 06:17:28 +0000

    fix line length

--- tests/conftest.py
+++ tests/conftest.py
@@ -29,7 +29,9 @@ def valid_user() -> Generator[None, None, None]:
 def adaptavist(valid_user: None) -> Generator[Tuple[MagicMock, MagicMock, MagicMock], None, None]:
     """Patch adapvavist to prevent real I/O."""
     with patch("adaptavist.Adaptavist.get_test_result", return_value={"scriptResults": [{"status": "Pass", "index": "0"}], "status": "Pass"}), \
-         patch("adaptavist.Adaptavist.get_test_run", return_value={"items": [{"testCaseKey": "TEST-T123"}, {"testCaseKey": "TEST-T124"}, {"testCaseKey": "TEST-T121"}]}), \
+         patch("adaptavist.Adaptavist.get_test_run", return_value={"items": [{"testCaseKey": "TEST-T123"},
+                                                                             {"testCaseKey": "TEST-T124"},
+                                                                             {"testCaseKey": "TEST-T121"}]}), \
          patch("adaptavist.Adaptavist.get_test_cases", return_value=[{"key": "TEST-T123"}]), \
          patch("adaptavist.Adaptavist.get_test_run_by_name", return_value={"key": "TEST_RUN_TEST"}), \
          patch("adaptavist.Adaptavist.get_test_case", return_value={"name": "TEST-T123", "priority": "Normal"}), \
