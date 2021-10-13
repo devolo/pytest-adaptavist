2bc458da16112f0ee80a4adbca24efec065e0d1e
Markus Bong <Markus.Bong@devolo.de>
2021-10-13 10:02:20 +0000

    add more tests

--- tests/unit_tests/test_pytest_adaptavist.py
+++ tests/unit_tests/test_pytest_adaptavist.py
@@ -1,7 +1,7 @@
 """ Test connection between pytest and Adaptavist."""
 
 from typing import Tuple
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
 import pytest
 
@@ -55,3 +55,52 @@ def test_early_return_on_no_config(pytester: pytest.Pytester, adaptavist: Tuple[
     assert ctr.call_count == 0
     assert etrs.call_count == 0
     assert etss.call_count == 0
+
+
+def test_skipped_test_cases(pytester: pytest.Pytester, adaptavist: Tuple[MagicMock, MagicMock, MagicMock]):
+    """Test that a skipped test case is reported as 'Not Executed'."""
+    pytester.makepyfile("""
+        import pytest
+
+        @pytest.mark.skip()
+        def test_T123(meta_block):
+            with meta_block(1):
+                assert True
+    """)
+    with patch("adaptavist.Adaptavist.get_test_result", return_value={"scriptResults": [{"index": "0"}]}):
+        pytester.runpytest("--adaptavist")
+    _, etrs, _ = adaptavist
+    assert etrs.call_count == 1
+    assert etrs.call_args.kwargs["status"] == "Not Executed"
+
+
+def test_bcd(pytester: pytest.Pytester, adaptavist: Tuple[MagicMock, MagicMock, MagicMock]):
+    """Test that a testcase is reported correctly. The steps must not reported in this test."""
+    pytester.makepyfile("""
+        import pytest
+
+        def test_T123(meta_block):
+            with meta_block() as mb:
+                mb.check(True)
+    """)
+    pytester.runpytest("--adaptavist")
+    _, etrs, _ = adaptavist
+    assert etrs.call_count == 1
+    assert etrs.call_args.kwargs["status"] == "Pass"
+
+
+@pytest.mark.usefixtures("adaptavist")
+def test_test_result_attachment(pytester: pytest.Pytester):
+    """Test that an attachment is correctly attached to the testcase (not to the step)."""
+    pytester.makepyfile("""
+        import pytest
+
+        def test_T123(meta_block):
+            with meta_block() as mb:
+                mb.check(True, attachment="ATTACHMENT", filename="test.txt")
+    """)
+    with patch("adaptavist.Adaptavist.add_test_result_attachment") as atra:
+        pytester.runpytest("--adaptavist")
+    assert atra.call_count == 1
+    assert atra.call_args.kwargs["attachment"] == "ATTACHMENT"
+    assert atra.call_args.kwargs["filename"] == "test.txt"
