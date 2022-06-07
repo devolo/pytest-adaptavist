a6e6b2320cfdda4c55830ccd3b8ce04ff35ed79d
Markus Bong <Markus.Bong@devolo.de>
2022-06-07 09:32:24 +0200

    fix jira strings test

--- tests/test_ini.py
+++ tests/test_ini.py
@@ -20,9 +20,6 @@ class TestIniConfig:
             "test_run_folder",
             "test_run_key",
             "test_run_suffix",
-            "jira_server",
-            "jira_username",
-            "jira_password",
         ],
     )
     def test_ini_config_strings(self, pytester: pytest.Pytester, monkeypatch: pytest.MonkeyPatch, option: str):
@@ -57,3 +54,54 @@ class TestIniConfig:
             "C2",
             ["C2"],
         )  # pylint: disable=protected-access
+
+    @pytest.mark.parametrize(
+        "option",
+        [
+            "jira_username",
+            "jira_password",
+        ],
+    )
+    def test_jira_username_and_password(self, pytester: pytest.Pytester, monkeypatch: pytest.MonkeyPatch, option: str):
+        """Test that jira username and password string values in pytest.ini are correctly used and recognized by pytest."""
+        monkeypatch.setenv("PYTEST_DISABLE_PLUGIN_AUTOLOAD", "1")
+        pytester.makepyfile(
+            """
+                def test_T1(meta_block):
+                    with meta_block(1) as mb_1:
+                        mb_1.check(True)
+            """
+        )
+        pytester.makeini(
+            f"""
+            [pytest]
+            {option} = C1
+            """
+        )
+        report = pytester.inline_run("--adaptavist", plugins=["adaptavist", "assume"])
+        adaptavist = report._pluginmanager.get_plugin("_adaptavist")
+        assert (
+            getattr(adaptavist.adaptavist._authentication, option.split("_")[-1]) == "C1"
+        )  # pylint: disable=protected-access
+
+    def test_jira_server(self, pytester: pytest.Pytester, monkeypatch: pytest.MonkeyPatch):
+        """Test that the jira_server string values in pytest.ini is correctly used and recognized by pytest."""
+        option = "jira_server"
+
+        monkeypatch.setenv("PYTEST_DISABLE_PLUGIN_AUTOLOAD", "1")
+        pytester.makepyfile(
+            """
+                def test_T1(meta_block):
+                    with meta_block(1) as mb_1:
+                        mb_1.check(True)
+            """
+        )
+        pytester.makeini(
+            f"""
+            [pytest]
+            {option} = C1
+            """
+        )
+        report = pytester.inline_run("--adaptavist", plugins=["adaptavist", "assume"])
+        adaptavist = report._pluginmanager.get_plugin("_adaptavist")
+        assert getattr(adaptavist.adaptavist, option) == "C1"  # pylint: disable=protected-access
