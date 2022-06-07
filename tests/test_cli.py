85cb28a74f0660e86417efd77f2058eacd5cf479
Markus Bong <Markus.Bong@devolo.de>
2022-06-07 14:01:52 +0200

    fix cycle information (#45)
    * fix cycle information
    
    * Changelog
    
    * remove unneeded fixture
    
    * fix Changelog
    
    * isort

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,7 @@
 """Test CLI behavior."""
 import pytest
 from _pytest.config import ExitCode
+from _pytest.pytester import LineMatcher
 
 
 @pytest.mark.usefixtures("configure")
@@ -27,7 +28,26 @@ class TestCliUnit:
         config = pytester.parseconfig()
         assert config.getini("adaptavist")
 
-    @pytest.mark.usefixtures("adaptavist_mock")
+    def test_cycle_info_urls(self, pytester: pytest.Pytester):
+        """Test that the cycle information urls are build correctly."""
+        pytester.makepyfile(
+            """
+            import pytest
+
+            def test_TEST_T123():
+                assert True
+        """
+        )
+        report = pytester.runpytest("--adaptavist")
+        matcher = LineMatcher(report.outlines)
+        matcher.fnmatch_lines(
+            [
+                "traceability:  https://jira.test/secure/Tests.jspa#/reports/traceability/report/view?tql=testResult.projectKey%20IN%20%28%22TEST%22%29%20AND%20testRun.key%20IN%20%28%22TEST-C1%22%29%20AND%20testRun.onlyLastTestResult%20IS%20true&jql=&title=REPORTS.TRACEABILITY_REPORT.TITLE&traceabilityReportOption=COVERAGE_TEST_CASES&traceabilityTreeOption=COVERAGE_TEST_CASES&traceabilityMatrixOption=COVERAGE_TEST_CASES&period=MONTH&scorecardOption=EXECUTION_RESULTS",
+                "test_summary:  https://jira.test/secure/Tests.jspa#/reports/testresults/board/view?tql=testResult.projectKey%20IN%20%28%22TEST%22%29%20AND%20testRun.key%20IN%20%28%22TEST-C1%22%29%20AND%20testRun.onlyLastTestResult%20IS%20true&jql=&title=REPORTS.TEST_RESULTS_BOARD.TITLE&traceabilityReportOption=COVERAGE_TEST_CASES&traceabilityTreeOption=COVERAGE_TEST_CASES&traceabilityMatrixOption=COVERAGE_TEST_CASES&period=MONTH&scorecardOption=EXECUTION_RESULTS",
+                "score_matrix:  https://jira.test/secure/Tests.jspa#/reports/testresults/scorecard/coverage/view?tql=testResult.projectKey%20IN%20%28%22TEST%22%29%20AND%20testRun.key%20IN%20%28%22TEST-C1%22%29%20AND%20testRun.onlyLastTestResult%20IS%20true&jql=&title=REPORTS.TEST_RESULTS_SCORECARD_BY_COVERAGE.TITLE&traceabilityReportOption=COVERAGE_TEST_CASES&traceabilityTreeOption=COVERAGE_TEST_CASES&traceabilityMatrixOption=COVERAGE_TEST_CASES&period=MONTH&scorecardOption=EXECUTION_RESULTS",
+            ]
+        )
+
     def test_invalid_branch(self, pytester: pytest.Pytester, monkeypatch: pytest.MonkeyPatch):
         """Test the correct behavior of an invalid branch."""
         pytester.makepyfile(
